4afed0eee5ddf7cf5bb9eab0a41e1d6708661ef7
paavkar <paavo_karppinen@outlook.com>
2023-11-26 22:32:49 +0200

    Password reset code and reset link email sending

--- Cinereg/Components/Account/IdentityNoOpEmailSender.cs
+++ Cinereg/Components/Account/IdentityNoOpEmailSender.cs
@@ -29,7 +29,7 @@ namespace Cinereg.Components.Account
             var regex = new Regex(Regex.Escape(";"));
             _email = _config["EmailSender:Email"];
             _password = _config["EmailSender:Password"];
-            confirmationLink = regex.Replace(confirmationLink, "&", 1);
+            confirmationLink = regex.Replace(confirmationLink, "&", confirmationLink.Count(x => x == ';'));
             System.Net.NetworkCredential credentials = new(_email, _password);
             var sender = new SmtpSender(() => new SmtpClient("smtp-mail.outlook.com", 587)
             {
@@ -42,7 +42,6 @@ namespace Cinereg.Components.Account
             Email.DefaultSender = sender;
             Email.DefaultRenderer = new RazorRenderer();
             StringBuilder template = new();
-            //var template = "Dear @Model.Name, You are totally @Model.Compliment.";
             template.AppendLine("Hey @Model.Email,");
             template.AppendLine("<p>Please confirm your account by <a href='@Model.ConfirmationLink'>clicking here</a>.</p>");
             template.AppendLine("- Developer of Cinereg");
@@ -59,10 +58,69 @@ namespace Cinereg.Components.Account
             //return emailSender.SendEmailAsync(email, "Confirm your email", $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
         }
 
-        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink) =>
-            emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+        {
+            _email = _config["EmailSender:Email"];
+            _password = _config["EmailSender:Password"];
+            System.Net.NetworkCredential credentials = new(_email, _password);
+            var sender = new SmtpSender(() => new SmtpClient("smtp-mail.outlook.com", 587)
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = credentials,
+                EnableSsl = true
+            });
 
-        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode) =>
-            emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password using the following code: {resetCode}");
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+            StringBuilder template = new();
+            template.AppendLine("Hey @Model.Email,");
+            template.AppendLine("<p>Please reset your password by <a href='@Model.ResetLink'>clicking here</a>.</p>");
+            template.AppendLine("- Developer of Cinereg");
+            template.AppendLine("<p>You can ignore this email if this was not you</p>");
+
+            var emailToSend = Email
+                .From("paavo_karppinen_dev@outlook.com")
+                .To(email)
+                .Subject("Reset your password")
+                .UsingTemplate(template.ToString(), new { Email = email, ResetLink = resetLink })
+                .Send();
+
+            return Task.CompletedTask;
+            //return emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+        }
+        
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+        {
+            _email = _config["EmailSender:Email"];
+            _password = _config["EmailSender:Password"];
+            System.Net.NetworkCredential credentials = new(_email, _password);
+            var sender = new SmtpSender(() => new SmtpClient("smtp-mail.outlook.com", 587)
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = credentials,
+                EnableSsl = true
+            });
+
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+            StringBuilder template = new();
+            template.AppendLine("Hey @Model.Email,");
+            template.AppendLine("<p>Please reset your password using the following code: @Model.ResetCode</p>");
+            template.AppendLine("- Developer of Cinereg");
+            template.AppendLine("<p>You can ignore this email if this was not you</p>");
+
+            var emailToSend = Email
+                .From("paavo_karppinen_dev@outlook.com")
+                .To(email)
+                .Subject("Reset your password")
+                .UsingTemplate(template.ToString(), new { Email = email, ResetCode = resetCode })
+                .Send();
+
+            return Task.CompletedTask;
+            //return emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password using the following code: {resetCode}");
+        }
+            
     }
 }
